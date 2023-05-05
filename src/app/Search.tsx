7a94d3a56b25be6b823b7cfa6495994d43932a24
kenvilar <kenvilar@gmail.com>
2023-05-05 17:35:27 +0800

    implement search component

---
+++ src/app/Search.tsx
@@ -0,0 +1,37 @@
+const Search = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-between space-y-5 md:flex-row md:space-y-0">
+        <div className="flex justify-between border-b">
+          <input
+            type="search"
+            className="ml-6 border-none md:w-80 placeholder:font-thin focus:outline-none"
+            placeholder="Search"
+          />
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 text-gray-300 duration-200 hover:scale-110"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="10" cy="10" r="7" />
+              <line x1="21" y1="21" x2="15" y2="15" />
+            </svg>
+          </button>
+        </div>
+
+        <button className="py-3 px-14 text-lg font-normal text-white bg-black border border-black rounded-md shadow-2xl duration-200 hover:bg-white hover:text-black">
+          Upload
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Search;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Menu from "@/app/Menu";
+import Search from "@/app/Search";
 
 export default function Home() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-cyan-400">
+      <div className="flex items-center justify-center min-h-screen bg-cyan-50">
         <div className="bg-white p-6 m-3 space-y-10 shadow-2xl rounded-3xl md:p-40">
           <Menu />
+
+          <Search />
         </div>
       </div>
     </>
